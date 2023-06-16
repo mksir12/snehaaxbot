3c6091d44ea094ea442d103c791a4cc769943843
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-16 10:42:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,6 +16,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
+BOT_NAME = environ['BOT_NAME']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
