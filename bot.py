52c58d9f547c4b701b79589cd6808bbe096dc24c
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-06-20 10:41:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from Script import script 
+from assistbot import assist
 from datetime import date, datetime 
 import pytz
 
