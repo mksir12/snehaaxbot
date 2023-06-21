26c8a481180c1081789e6261361809dafd82f8bb
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-21 16:43:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mksir12/testingdq.git /DQTheFileDonorBot
+  git clone https://github.com/mksir12/spidey.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
