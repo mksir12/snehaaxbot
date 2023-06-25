759482bd9be416a5ffe192fb0629564d31f18ef7
Cybermon3g <134679332+Cybermon3g@users.noreply.github.com>
2023-06-25 12:50:00 +0530

    Update search.py

--- plugins/AFSU/search.py
+++ plugins/AFSU/search.py
@@ -10,5 +10,5 @@ from plugins.AFSU.engine import ask_ai
 async def openai_ask(client, message):
     if len(message.command) == 1:
        return await message.reply_text("Give an input!")
-    m = await message.reply_text("❓")
+    m = await message.reply_text("Looking👀")
     await ask_ai(client, m, message)
